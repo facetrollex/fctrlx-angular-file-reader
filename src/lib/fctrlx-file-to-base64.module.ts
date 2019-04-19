ea3306f820d38fa1b720329047fe76acdcfa1d57
Facetrollex <alexey.khamitsevich@gmail.com>
2019-04-19 14:34:26 +0300

    Added convert to text directive

--- src/lib/fctrlx-file-to-base64.module.ts
+++ src/lib/fctrlx-file-to-base64.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FctrlxFileToBase64Directive } from './fctrlx-file-to-base64.directive';
+import { FctrlxFileToTextDirective } from './fctrlx-file-to-text.directive';
 
 @NgModule({
   imports: [],
-  declarations: [FctrlxFileToBase64Directive],
-  exports: [FctrlxFileToBase64Directive],
+  declarations: [FctrlxFileToBase64Directive, FctrlxFileToTextDirective],
+  exports: [FctrlxFileToBase64Directive, FctrlxFileToTextDirective],
 })
 export class FctrlxFileToBase64 { }
---
+++ src/lib/fctrlx-file-to-text.directive.ts
@@ -0,0 +1,27 @@
+import {
+    Directive,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+} from '@angular/core';
+import { Base } from './Base';
+
+@Directive({
+    selector: '[fileToText]',
+})
+export class FctrlxFileToTextDirective extends Base {
+  @Input() files: any;
+  @Input() type: string;
+  @Input() multiple: undefined | null | string | boolean;
+
+  @Output() filesChange: EventEmitter<any> = new EventEmitter();
+
+  constructor(element: ElementRef) {
+    super('fileToText', element);
+  }
+
+  filesChanged(event: Event): void {
+    super.filesChanged(event, 'readAsText', 'text');
+  }
+}
