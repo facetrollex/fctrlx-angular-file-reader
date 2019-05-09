3ae14a2c160018a711d7cab1912320f73722fdb1
Facetrollex <alexey.khamitsevich@gmail.com>
2019-05-10 01:48:04 +0300

    Fixed Module visibility problem

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,3 +3,8 @@
  */
 
 export * from './lib/fctrlx-angular-file-reader.module';
+export * from './lib/fctrlx-file-to-base64.directive';
+export * from './lib/fctrlx-file-to-text.directive';
+export * from './lib/fctrlx-file-to-array-buffer.directive';
+export * from './lib/Base';
+export * from './lib/converted';
